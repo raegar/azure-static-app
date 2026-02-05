750295b726dd34df9101965cbf386acb8469c615
raegar <jamie.myland@gmail.com>
2026-02-05 12:16:35 +0000

    Added functions

--- api/Quotes/index.js
+++ api/Quotes/index.js
@@ -1,80 +1,32 @@
 const { BlobServiceClient } = require("@azure/storage-blob");
 
-
-
-
 module.exports = async function (context, req) {
-
   const blobServiceClient = BlobServiceClient.fromConnectionString(
-
     process.env.AzureWebJobsStorage
-
   );
 
-
-
-
   const containerClient = blobServiceClient.getContainerClient("quotes");
-
   const blobClient = containerClient.getBlobClient("quotes.json");
 
-
-
-
   const downloadResponse = await blobClient.download();
-
   const downloaded = await streamToString(downloadResponse.readableStreamBody);
 
-
-
-
   const data = JSON.parse(downloaded);
-
   const quotes = data.quotes;
-
-
-
 
   const randomIndex = Math.floor(Math.random() * quotes.length);
 
-
-
-
   context.res = {
-
-    body: {
-
-      quote: quotes[randomIndex]
-
-    }
-
+    headers: { "Content-Type": "application/json" },
+    body: { quote: quotes[randomIndex] }
   };
-
 };
 
-
-
-
 async function streamToString(readableStream) {
-
   return new Promise((resolve, reject) => {
-
     const chunks = [];
-
-    readableStream.on("data", (data) => {
-
-      chunks.push(data.toString());
-
-    });
-
-    readableStream.on("end", () => {
-
-      resolve(chunks.join(""));
-
-    });
-
+    readableStream.on("data", (data) => chunks.push(data.toString()));
+    readableStream.on("end", () => resolve(chunks.join("")));
     readableStream.on("error", reject);
-
   });
-
 }
